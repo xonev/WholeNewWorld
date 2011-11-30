7cc1e49f1917de483468a32f66ef3ace775d08e4
Steven Oxley <steven.aj.oxley@gmail.com>
2011-11-29 22:25:33 -0600

    Changed database.js to use mongoose instead of the super-unfriendly mongo driver.

--- database.js
+++ database.js
@@ -1,69 +1,43 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/race_index');
+
+var raceSchema = new mongoose.Schema({
+    name: { type: String, index: true },
+    thumbnail: { type: String },
+    image: { type: String },
+    description: { type: String },
+    attributes:  [String],
+});
+
+mongoose.model('Race', raceSchema);
+    
 module.exports = function (host, port) {
     return (function () {
-        var db = new mongo.Db(
-            'test',
-            new mongo.Server(host, port, { auto_reconnect: true }, {})
-        );
-            
-        function openConnection(callback) {
-            if (db.state === "notConnected") {
-                db.open(function (err, db) {
-                    if (err === null) {
-                        callback(null, db);
-                    }
-                    else {
-                        console.log(err);
-                        callback(err, null);
-                    }
-                });
-            }
-            else {
-                callback(null, db);
-            }
-        }
-
         function getRaceByName(name, callback) {
-            openConnection(function (err, db) {
+            var race = mongoose.model('Race');
+            race.find({'name': name}, function (err, races) {
                 if (err !== null) {
+                    console.log(err);
                     callback(null);
                 }
+                else if (races.length > 0) {
+                    callback(races[0]);
+                }
                 else {
-                    db.collection('races', function (err, races) {
-                        var cursor = races.find({'name': name});
-                        cursor.nextObject(function (err, obj) {
-                            if (err === null) {
-                                callback(obj);
-                            }
-                            else {
-                                console.log(err);
-                                callback(null);
-                            }
-                        });
-                    });
+                    callback(null);
                 }
             });
         }
 
         function getRaces(callback) {
-            openConnection(function (err, db) {
-                if (err !== null) {
-                    callback(null);
+            var race = mongoose.model('Race');
+            race.find({}, ['name', 'thumbnail'], function (err, races) {
+                if (err === null) {
+                    callback(races);
                 }
                 else {
-                    db.collection('races', function (err, races) {
-                        var cursor = races.find(
-                            {},
-                            {'name': true, 'thumbnail': true});
-                        cursor.toArray(function (err, raceList) {
-                            if (err === null) {
-                                callback(raceList);
-                            }
-                            else {
-                                console.log(err);
-                                callback([]);
-                            }
-                        });
-                    });
+                    console.log(err);
+                    callback([]);
                 }
             });
         }
